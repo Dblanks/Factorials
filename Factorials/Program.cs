61cae1f4aa63d0797c31a29e32d9c04af59b1dbc
Dom Blanks <dblanks948@gmail.com>
2019-06-15 20:41:15 -0500

    Factorials of a number application.

--- Factorials/Program.cs
+++ Factorials/Program.cs
@@ -10,12 +10,46 @@ namespace Factorials
     {
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            //Prompt user for a number
+            Console.WriteLine("Enter a number to retrieve it's factorials: ");
+            int userInput = Convert.ToInt32(Console.ReadLine()); //accept user input assuming a valid int is provided
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+            //invoke get factorials method
+            GetFactorials(userInput);
+            Console.Read();
+        }
+
+        private static void GetFactorials(int userInput)
+        {
+            //set product to 1 initially and not 0
+            int product = 1;
+
+            //Create a string builder for building the factorial string 
+            StringBuilder sb = new StringBuilder();
+             
+            //start the string builder with the user's number input
+            sb.Append(userInput + "!=");
+
+            //loop and get every number below the user's input
+            for(int i = userInput - 1; i > 0; i--)
+            {
+                if (i == 1)
+                {
+                    sb.Append(i + "=");
+                }
+                else
+                {
+                    sb.Append(i + "*");
+                }
+               
+                product *= i;
+            }
+
+            //calculate the product of numbers looped multiplied by the number supplied
+            product = (product * userInput);
+
+            //Write to console the stringbuilder as the message containing the factorials and product
+            Console.Write(sb.Append(product));
         }
     }
 }
